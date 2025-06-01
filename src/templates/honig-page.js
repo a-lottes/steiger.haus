712124fba82a103a0327fa39cb84f924ab8a98c7
netzwerkwelten <realbyroncipher@gmail.com>
2025-06-01 17:57:58 +0200

    test

--- src/templates/honig-page.js
+++ src/templates/honig-page.js
@@ -72,7 +72,7 @@ query IndexPage {
     
   }
   allMarkdownRemark(
-    filter: {frontmatter: {templateKey: {eq: "work-sub-page"}}}
+    filter: {frontmatter: {templateKey: {eq: "honig-sub-page"}}}
     limit: 30
     sort: {frontmatter: {date: DESC}}
   ) {
